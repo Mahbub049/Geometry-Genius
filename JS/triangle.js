a0ead7ce7e36c0dff0a5e7b5ed24803e998a9a7b
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-02-14 01:26:15 +0600

    bugs but not resolved

--- JS/triangle.js
+++ JS/triangle.js
@@ -1,14 +1,33 @@
-document.getElementById('triangle-calculate').addEventListener('click', function(event){
+const triangle = document.getElementById('triangle-calculate').addEventListener('click', function(event){
     const b = parseFloat(document.getElementById('triangle-b').value);
     const h = parseFloat(document.getElementById('triangle-h').value);
 
     document.getElementById('baseHeight').innerText = 'b: '+b +' cm'+ ' h: '+h + ' cm';
 
     if(isNaN(b) || isNaN(h)){
-        console.log('Please add both values');
+        return 'Please add both values';
     }
     else{
-        const area = 0.5 * b * h;
-        console.log(area);
+        let area = 0.5 * b * h;
+        const list = document.getElementById('list');
+        const li = document.createElement('li');
+        const value = 'Triangle <span class="pr-9"></span> <span id="areaUpdate">' + area + '</span><span id="changeCMtoM">cm</span><sup>2</sup> <span class="pr-9"></span>';
+        
+        const Button = document.createElement("button");
+        Button.innerHTML = 'Convert to m<sup>2</sup>'; 
+        Button.className = "mr-10 bg-[#1090D8] rounded-md text-white font-medium text-sm px-4 py-2";
+        Button.id= "convert-m";
+        Button.addEventListener("click", function(){
+            const updatedArea = area * 0.01;
+            document.getElementById('areaUpdate').innerText = updatedArea;
+            document.getElementById('changeCMtoM').innerText = 'm';
+        });
+
+        // Append the button as a child to the li element
+        li.innerHTML = value;
+        li.appendChild(Button);
+        
+        list.appendChild(li);
+        
     }
 })
